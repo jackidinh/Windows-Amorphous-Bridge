8e6f17b904a82ab6a3fdcb65551ea5254f71537d
L3HARRIS\L3Har <mark.snyder@l3harris.com>
2025-08-05 14:16:25 -0400

    working proto for proto system

--- ZmqPublisher/ZmqPublisher/ZmqPublisher.cpp
+++ ZmqPublisher/ZmqPublisher/ZmqPublisher.cpp
@@ -2,6 +2,10 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include "person.pb.h"
+#include <string>
+
+
 
 int main() {
     zmq::context_t context(1);
@@ -9,12 +13,17 @@ int main() {
 
     publisher.bind("tcp://*:5555");
 
+    Person olivia;
+    olivia.set_name("Olivia");
+    olivia.set_id(9);
+
+    std::string serialized;
+    olivia.SerializeToString(&serialized);
+
     int count = 0;
     while (true) {
-        std::string message = "Hello ZeroMQ " + std::to_string(count++);
-        zmq::message_t zmq_msg(message.c_str(), message.size());
-        publisher.send(zmq_msg, zmq::send_flags::none);
-        std::cout << "Sent: " << message << std::endl;
+        publisher.send(zmq::buffer(serialized), zmq::send_flags::none);
+        std::cout << "Sent Protobuf Message: " << olivia.name()<<" with id: "<<olivia.id() << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
     return 0;
